2b1769689b454f9b39398ec4dfa24f38ff60bab7
whythat <yuri.zhykin@gmail.com>
2017-02-02 23:56:27 +0200

    API draft

--- api/endpoints.go
+++ api/endpoints.go
@@ -1,5 +1,15 @@
 package net
 
+import "github.com/yurizhykin/peernotify/core"
+
 type jsonAPIHandler struct {
 	node *core.PeerNotifyNode
+}
+
+func handleRegister() {
+
+}
+
+func handleForward() {
+
 }
