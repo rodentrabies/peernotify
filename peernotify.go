2ed4bd46c1a8745b2ec9767f0ad3efa55c49312b
whythat <yuri.zhykin@gmail.com>
2017-02-03 23:48:46 +0200

    API first steps

--- peernotify.go
+++ peernotify.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/yurizhykin/peernotify/api"
+)
 
 func main() {
 	fmt.Println("Starting peernotify server...")
+	api.APIServe(2701)
+	http.ListenAndServe(":2701", nil)
 }
