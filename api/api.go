8f5d0492a2cd2cd09ce78b389c1b21fd1c99b188
whythat <yuri.zhykin@gmail.com>
2017-02-07 23:24:21 +0200

    Use httprouter lib for routing

--- api/api.go
+++ api/api.go
@@ -3,61 +3,32 @@ package api
 import (
 	"fmt"
 	"net/http"
-	"net/url"
 
 	"github.com/golang/protobuf/jsonpb"
+	r "github.com/julienschmidt/httprouter"
 	"github.com/yurizhykin/peernotify/core"
 	"github.com/yurizhykin/peernotify/pb"
 )
 
-const (
-	API_PREFIX  = "" // "/api/"
-	API_VERSION = "" // "v1"
-)
-
 type apiHandler struct {
-	mux  map[string]func(http.ResponseWriter, *http.Request)
+	*r.Router
 	node *core.PeernotifyNode
 }
 
 func NewAPIServer(node *core.PeernotifyNode, addr string) http.Server {
-	h := newAPIHandler(node)
-	h.register(map[string]func(http.ResponseWriter, *http.Request){
-		"/register": h.handleRegister,
-		"/verify":   h.handleVerify,
-		"/forward":  h.handleForward,
-	})
+	h := &apiHandler{r.New(), node}
+
+	// Register API handlers
+	h.POST("/register", h.Register)
+	h.GET("/verify", h.Verify)
+	h.POST("/forward", h.Forward)
+
 	return http.Server{Addr: addr, Handler: h}
 }
 
-func (h *apiHandler) register(handlers map[string]func(http.ResponseWriter, *http.Request)) {
-	for path, handler := range handlers {
-		h.mux[API_PREFIX+API_VERSION+path] = handler
-	}
-}
-
-func newAPIHandler(node *core.PeernotifyNode) *apiHandler {
-	return &apiHandler{make(map[string]func(http.ResponseWriter, *http.Request)), node}
-}
-
-func (h *apiHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	u, err := url.Parse(r.URL.Path)
-	if err != nil {
-		http.Error(w, "Malformed URL", 400)
-	}
-	path := u.String()
-	if handler, ok := h.mux[path]; !ok {
-		apiNotFound(w, r)
-	} else {
-		handler(w, r)
-	}
-}
-
-func (h *apiHandler) handleRegister(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		apiWrongMethod(w, r)
-		return
-	}
+//------------------------------------------------------------------------------
+// API handlers
+func (h *apiHandler) Register(w http.ResponseWriter, r *http.Request, _ r.Params) {
 	var contact pb.Contact
 	if err := jsonpb.Unmarshal(r.Body, &contact); err != nil {
 		apiWrongBody(w, r)
@@ -69,19 +40,16 @@ func (h *apiHandler) handleRegister(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func (h *apiHandler) handleVerify(w http.ResponseWriter, r *http.Request) {
-	apiNotFound(w, r)
+func (h *apiHandler) Verify(w http.ResponseWriter, r *http.Request, _ r.Params) {
+
 }
 
-func (h *apiHandler) handleForward(w http.ResponseWriter, r *http.Request) {
-	apiNotFound(w, r)
+func (h *apiHandler) Forward(w http.ResponseWriter, r *http.Request, _ r.Params) {
+
 }
 
+//------------------------------------------------------------------------------
 // HTTP error responses
-func apiNotFound(w http.ResponseWriter, r *http.Request) {
-	http.NotFound(w, r)
-}
-
 func apiWrongMethod(w http.ResponseWriter, r *http.Request) {
 	http.Error(w, fmt.Sprintf("Unable to %s", r.Method), 405)
 }
