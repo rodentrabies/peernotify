cbcd2d6ccaa6a49b2cced41a854dbb6696d4d145
whythat <yuri.zhykin@gmail.com>
2017-02-14 03:11:11 +0200

    Small changes to node structure

--- core/node.go
+++ core/node.go
@@ -7,33 +7,72 @@ import (
 )
 
 type PeernotifyNode struct {
+	// Pair of keys used to communicate/authenticate
+	// peernotify node.
 	KeyPair KeyPair
-	Storage storage.Storage
+
+	// Storage for unconfirmed contacts key-value pairs of
+	// the form [<random key> -> <contact data>]. When contact
+	// is verified, its data is removed from pending storage and
+	// put into contacts storage.
+	Pending storage.Storage
+
+	// Permanent contacts storage. Contact data is moved here after
+	// confirmation and all lookups are made in this storage.
+	Contacts storage.Storage
 }
 
 func NewPeernotifyNode(storefile string) (*PeernotifyNode, error) {
 	keys := NewKeyPair()
-	store, err := storage.NewStorage(storefile)
+	// TODO: move into one storage (?)
+	pendingStore, err := storage.NewStorage("/tmp/peernotify.pending")
+	contactStore, err := storage.NewStorage(storefile)
 	if err != nil {
 		return nil, err
 	}
-	return &PeernotifyNode{KeyPair: keys, Storage: store}, nil
+	return &PeernotifyNode{
+		KeyPair:  keys,
+		Pending:  pendingStore,
+		Contacts: contactStore,
+	}, nil
 }
 
-func (n *PeernotifyNode) storeContact(contact *pb.Contact) ([]byte, error) {
+// Store contact data in temporary storage
+func (n *PeernotifyNode) registerContact(key []byte, contact *pb.Contact) error {
+	return storeContact(n.Contacts, key, contact)
+}
+
+// Store contact data in permanent storage after verification
+func (n *PeernotifyNode) saveContact(key []byte, contact *pb.Contact) error {
+	return storeContact(n.Pending, key, contact)
+}
+
+func storeContact(st storage.Storage, key []byte, contact *pb.Contact) error {
 	contactBytes, err := proto.Marshal(contact)
 	if err != nil {
-		return nil, err
+		return err
 	}
-	contactKey := ContactKey(contactBytes)
-	if err := n.Storage.Store(contactKey, contactBytes); err != nil {
-		return nil, err
+	if err := st.Store(key, contactBytes); err != nil {
+		return err
 	}
-	return contactKey, nil
+	return nil
 }
 
+// Get contact data from pending storage
+func (n *PeernotifyNode) getPendingContact(key []byte) (*pb.Contact, error) {
+	contactBytes, err := n.Pending.Get(key)
+	var contact pb.Contact
+	proto.Unmarshal(contactBytes, &contact)
+	if err != nil {
+		return nil, err
+	}
+	return &contact, nil
+}
+
+// Get contact data from permanent storage
+// NOTE: should implement cryptographic mechanism of token generation
 func (n *PeernotifyNode) getContact(key []byte) (*pb.Contact, error) {
-	contactBytes, err := n.Storage.Get(key)
+	contactBytes, err := n.Contacts.Get(key)
 	var contact pb.Contact
 	proto.Unmarshal(contactBytes, &contact)
 	if err != nil {
