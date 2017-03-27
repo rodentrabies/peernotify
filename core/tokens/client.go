b84de50ed0b263eb72d752932b94b455e4c254bd
whythat <whythat@protonmail.com>
2017-03-27 07:47:57 +0300

    CryptoNote tokens implementation draft

--- core/tokens/client.go
+++ core/tokens/client.go
@@ -2,19 +2,23 @@ package tokens
 
 import "github.com/yurizhykin/monujo"
 
-// cnPNClient is a client adhering to the cryptonight-based peernotify protocol
-type cnPNClient struct{}
+// cnPNClient is a client adhering to the CryptoNote-based peernotify protocol
+type cryptoNoteClient struct{}
 
-func (*cnPNClient) NewToken(A, B Pubkey) (*Token, error) {
+func (*cryptoNoteClient) NewToken(A, B Pubkey) (*Token, error) {
 	r1, R1, err := monujo.NewKeyPair()
 	if err != nil {
 		return nil, err
 	}
-	Au := PubAdd(A, R1)
+	Au := addPublicKeys(A, R1)
 	X := encryptRandPrivkey(A, r1)
 	return &Token{Au, B, X}
 }
 
-func encryptRandPrivkey(A arbo.Pubkey, r1 arbo.Privkey) []byte {
+func encryptRandPrivkey(A Pubkey, r1 Privkey) []byte {
 	return []byte{}
+}
+
+func addPublicKeys(A, B Pubkey) Pubkey {
+	return nil
 }
--- core/tokens/manager.go
+++ core/tokens/manager.go
@@ -8,23 +8,23 @@ type cnTokenManager struct {
 }
 
 func NewTokenManager() (*TokenManager, error) {
-	wallet, err := arbo.NewWallet()
+	wallet, err := monujo.NewWallet()
 	wallet.Run(nil)
 	return &cnTokenManager{wallet: wallet}, nil
 }
 
 func (tm *cnTokenManager) NewContactKey() (Privkey, Pubkey, Privkey, Pubkey, error) {
-	a, A, err := arbo.NewKeyPair()
+	a, A, err := monujo.NewKeyPair()
 	if err != nil {
 		return nil, nil, nil, nil, err
 	}
-	b, B, err := arbo.NewKeyPair()
+	b, B, err := monujo.NewKeyPair()
 	if err != nil {
 		return nil, nil, nil, nil, err
 	}
 	return a, A, b, B, nil
 }
 
-func (tm *cnTokenManager) VerifyToken(token *Token) (UserID, error) {
-	return nil, nil
+func (tm *cnTokenManager) VerifyToken(token *Token) IdKey {
+	return token.UserIdKey
 }
