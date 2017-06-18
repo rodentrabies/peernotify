066d99ff14e0b4eb5f283b0930eb989cf340b88a
whythat <whythat@protonmail.com>
2017-06-18 19:07:06 +0300

    Implement forwarding QA tool

--- qa/forward.py
+++ qa/forward.py
@@ -3,10 +3,18 @@
 import requests
 import sys
 import json
+import base58
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
 
-def pn_forward(addr, token, payload):
+
+IDKEY_SIZE = 32
+
+
+def pn_forward(addr, keyset, payload):
     # construct message JSON description
-    message = {"token" : token, "payload" : payload}
+    token = generate_token(keyset)
+    message = {"token": token, "payload": payload}
     # expand URL
     if addr[0] == ':':
         addr = 'http://localhost' + addr
@@ -17,12 +25,22 @@ def pn_forward(addr, token, payload):
         r = requests.post(url_path, data=json.dumps(message))
         if not r.status_code == 200:
             print('[ERROR]: ' + r.reason)
+        else:
+            print('[TOKEN]: ' + token)
     except Exception as e:
         print('[ERROR]: ' + str(e))
+
+
+def generate_token(keyset):
+    data = base58.b58decode(keyset)
+    id_key, root_key = data[:IDKEY_SIZE], data[IDKEY_SIZE:]
+    digest = hashes.Hash(hashes.SHA256(), backend=default_backend())
+    digest.update(root_key)
+    return base58.b58encode(id_key + digest.finalize())
+
 
 if __name__ == '__main__':
     if len(sys.argv) != 4:
         print('[USAGE]:\n\tforward.py <addr> <token> <msg>')
     else:
         pn_forward(sys.argv[1], sys.argv[2], sys.argv[3])
-    
